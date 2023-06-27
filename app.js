f70c01f4df37e44c8d782d0dc7647764597a4c4c
Luana Baratta <luanabaratta.technology@gmail.com>
2023-06-27 14:28:37 -0300

    pdf generator created

--- app.js
+++ app.js
@@ -3,5 +3,22 @@ import * as fs from "fs";
 import * as pdf from 'html-pdf';
 
 const server = http.createServer((req, res) => {
-    let html = fs.readFileSync()
-})
+    let html = fs.readFileSync('./index.html');
+
+    const options = {
+        type: 'pdf',
+        format: 'A4',
+        orientation: 'portrait'
+    }
+
+    pdf.create(html.toString(), options).toBuffer((err, buffer) => {
+        if (err) return res.status(500).json(err);
+        res.end(buffer);
+    });
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server iniciado na porta ${port}`)
+});
